7fc7cff18a17a3becea6db7edcade722c23b1fe1
NaveenKumar Venkataraj <v.naveen.kr@gmail.com>
2024-03-29 13:30:42 +0530

    Change sw scope

--- sw.js
+++ sw.js
@@ -5,8 +5,9 @@ self.addEventListener('install', event => {
 	event.waitUntil((async () => {
 	const cache = await caches.open(CACHE_NAME);
 	cache.addAll([
-		'/',
-		'/index.html'
+		'/vbgl/',
+		'/vbgl/index.html',
+    '/vbgl/vbgl.png'
 	]);
 	})());
 });
